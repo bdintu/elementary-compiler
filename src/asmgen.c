15b145b330f8aa984b913de3c9b8491f9b4b686e
bdintu <bundit.jay@gmail.com>
2019-05-03 18:28:08 +0700

    Fix nest if countbranch

--- src/asmgen.c
+++ src/asmgen.c
@@ -436,19 +436,23 @@ void asmGen (struct ast* node) {
 
       if (((struct cond*)node)->tl) {
           asmGen(((struct cond*)node)->tl);
+      }
+      if (((struct cond*)node)->tr) {
           sprintf(buf, "L%u", condBranchNum+1);
           text = genText(text, newJump("jmp", buf));
       }
 
+      text = genText(text, genComment("else"));
       sprintf(buf, "L%u", condBranchNum);
       text = genText(text, newLabel(buf));
 
-      if (((struct cond*)node)->tr)
-          asmGen(((struct cond*)node)->tr);
+      if (((struct cond*)node)->tr) {
+                asmGen(((struct cond*)node)->tr);
+          ++condBranchNum;
+          sprintf(buf, "L%u", condBranchNum);
+          text = genText(text, newLabel(buf));
 
-      ++condBranchNum;
-      sprintf(buf, "L%u", condBranchNum);
-      text = genText(text, newLabel(buf));
+      }
 
       text = genText(text, add("$8", "%rsp"));
 
